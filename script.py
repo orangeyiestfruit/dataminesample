32940854a396d8cd918fb567047068d3266bff8e
Paul Brozenec <paulbrozenec@gmail.com>
2017-04-05 21:56:32 -0700

    New Functions
    Iterates through all possible demographics and tells you, based on the data, which demographic you should focus your resources on.

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
 data = []
 stats = []
 mlikes = 0
+import itertools
 from time import *
 #0 = dislike the company, 0 = not a teen, 0 = not female, 0 = no snapchat, 0 = republican
+
+
 """
 def gather():
     for y in range(0,100):
@@ -14,11 +17,13 @@ def gather():
             opin = int(input("Do you like Ford automanufacturers? Y = 1, N = 0"))
             comp = [opin,teen,gend,snap,part]
             print("Thank you for your input!")
-            sleep(2);
+            sleep(2);                 #just gathering manual input 
             data.append(comp)
         except:
             print("Please try again!")
             """
+
+
 def strip(filename):
     f = open(filename,"r")
     data = []
@@ -28,7 +33,6 @@ def strip(filename):
         fd = rt.split("-")
         data.append(list(map(int, fd)))
     return data
-print(strip("paul.txt"))
 data = strip("paul.txt")
 #data = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,0]]
 
@@ -43,15 +47,34 @@ def subcateg(subcat,value):
     try:
         return round((li/su),4)
     except:
-        return 0
-stats.append(subcateg(1,0))
-stats.append(subcateg(2,1))
-stats.append(subcateg(3,1))
-stats.append(subcateg(4,1))
-print(stats)
+        print("Insufficient Data.")
+def gener(one,two,thr,fou):
+    stats = []
+    stats.append(subcateg(1,one))
+    stats.append(subcateg(2,two))
+    stats.append(subcateg(3,thr))
+    stats.append(subcateg(4,fou))
+    return stats
+stats = gener(0,1,0,1)
+
 def gini(stat):
     r = 0
     for vals in stats:
         r+=(vals)*(vals)
     return 1-r/(len(data[0])-1)
-print(gini(stats))
+def iters():
+    #Don't tell me to clean this up.
+    als = list(itertools.product([0,1], repeat=4))  
+    todo = []
+    for fun in als:
+        #making als processable by gini function
+        fun = ",".join(str(fun))
+        fun = fun.replace(",","")
+        fun = fun.replace("(","")
+        fun = fun.replace(")","")
+        fun = fun.replace(" ","")
+        todo.append(gini(gener(fun[0],fun[1],fun[2],fun[3])))
+    return(max(todo))
+print(iters())
+print(gini(gener(0,0,0,0)))
+
